fd0f75be2d0507d550132ccdae60001b10f0e7a0
banditsbeware <david.rademacher@mavs.uta.edu>
2022-03-08 17:04:16 -0600

    from the darkness we grasp at sanity ⦿

--- src/apps/analysis/finalComponents/densityWeightedBipartiteGraphGenerator.py
+++ src/apps/analysis/finalComponents/densityWeightedBipartiteGraphGenerator.py
@@ -1,126 +1,87 @@
 import os
-def get_comm_info(community_id, community_dict):
-    community_id = str(community_id)
-    if community_id in community_dict:
-        return community_dict[community_id]
-    else:
-        return 0
 
-def densityWeightedBipartiteGraphGenerator(layer1CommunityFile,
-                                      layer2CommunityFile,
-                                      layer1IntraEdgeFile,
-                                      layer2IntraEdgeFile,
-                                      layer12SimpleEdgeBipartiteFile,
-                                      resultFile):
+def density_weighted_bgg(
+  layer1IntraEdgeFile, 
+  layer2IntraEdgeFile, 
+  layer1CommunityFile, 
+  layer2CommunityFile, 
+  interLayer12,
+  simpleEdgeBipartiteFile, 
+  resultFile
+):
 
-    director_community_info = []
-    director_community_dict = {}
-    movie_community_info = []
-    movie_community_dict = {}
-    no_of_vertices_in_movie_communities = {}
-    no_of_vertices_in_director_communities = {}
-    with open(layer1CommunityFile) as f:
-        for line in f:
-            director_community_info.append(line.split(' '))
-        for i in director_community_info:
-            if (len(i) == 3):
-                director_community_dict[i[0]] = i[1]
-                if i[1] not in no_of_vertices_in_director_communities:
-                    no_of_vertices_in_director_communities[i[1]] = 1
-                else:
-                    no_of_vertices_in_director_communities[i[1]]+=1
+  layer1community_dict = {}
+  layer1community_vertices = {}
+  layer1community_edges = {}
+    
+  layer2community_dict = {}
+  layer2community_vertices = {}
+  layer2community_edges = {}
 
-    with open(layer2CommunityFile) as f:
-        for line in f:
-            movie_community_info.append(line.split(' '))
-        for i in movie_community_info:
-            if (len(i) == 3):
-                movie_community_dict[i[0]] = i[1]
-                if i[1] not in no_of_vertices_in_movie_communities:
-                    no_of_vertices_in_movie_communities[i[1]] = 1
-                else:
-                    no_of_vertices_in_movie_communities[i[1]]+=1
+  i = 0
+  with open(layer1CommunityFile) as f:
+    for line in f:
+      i = i + 1
+      line_data = line.strip().split(',')
+      if (i >= 8):
+        layer1community_dict[line_data[0]] = line_data[1]
+        if line_data[1] not in layer1community_vertices:
+          layer1community_vertices[line_data[1]] = 1
+        else:
+          layer1community_vertices[line_data[1]]+=1
 
+  i = 0
+  with open(layer2CommunityFile) as f:
+    for line in f:
+      i = i + 1
+      line_data = line.strip().split(',')
+      if (i >= 8):
+        layer2community_dict[line_data[0]] = line_data[1]
+        if line_data[1] not in layer2community_vertices:
+          layer2community_vertices[line_data[1]] = 1
+        else:
+          layer2community_vertices[line_data[1]]+=1
 
+  with open(layer1IntraEdgeFile) as f:
+    for line in f:
+      line_data = line.strip().split(',')
+      if len(line_data) > 2:
+        if layer1community_dict[line_data[0]] == layer1community_dict[line_data[1]]:
+          if layer1community_dict[line_data[0]] not in layer1community_edges:
+            layer1community_edges[layer1community_dict[line_data[0]]] = 1
+          else:
+            layer1community_edges[layer1community_dict[line_data[0]]] += 1
 
+  with open(layer2IntraEdgeFile) as f:
+    for line in f:
+      line_data = line.strip().split(',')
+      if len(line_data) > 2:
+        if layer2community_dict[line_data[0]] == layer2community_dict[line_data[1]]:
+          if layer2community_dict[line_data[0]] not in layer2community_edges:
+            layer2community_edges[layer2community_dict[line_data[0]]] = 1
+          else:
+            layer2community_edges[layer2community_dict[line_data[0]]] += 1
 
-
-
-    def edge_density_weights(intraEdgeFile, communityFile):
-        graph_info = []
-        edge_info_dict = {}
-        community_info = []
-        no_of_edges_in_community = {}
-        community_dict = {}
-        edge_density_of_community = {}
-        with open(intraEdgeFile) as f:
-            for line in f:
-#                graph_info.append(line.split(' '))
-#            for i in graph_info:
-#                if len(i) > 2 :
-#                    if i[0] not in edge_info_dict:
-#                        edge_info_dict[i[0]] = [i[1]]
-#                    else:
-#                        edge_info_dict[i[0]].append(i[1])
-                line-data = line.split(' ')
-                if len(line-data) > 2 :
-                    if i[0] not in edge_info_dict:
-                        edge_info_dict[i[0]] = [i[1]]
-                    else:
-                        edge_info_dict[i[0]].append(i[1])
-
-        # storing the vertices belonging to each community
-        with open(communityFile) as f:
-            for line in f:
-                community_info.append(line.split(' '))
-            for i in community_info:
-                if (len(i) == 3):
-                    if i[1] not in community_dict:
-                        community_dict[i[1]] = [i[0]]
-                    else:
-                        community_dict[i[1]].append(i[0])
-
-
-        for community_id in community_dict:
-            temp =0
-            for vertices in community_dict[community_id]:
-                if vertices in edge_info_dict:
-                    for vertex in edge_info_dict[vertices]:
-                        if vertex in community_dict[community_id]:
-                            no_of_edges_in_community[community_id] = temp+1
-                            temp+=1
-        for comm_id in no_of_edges_in_community:
-            vertices_count = len(community_dict[comm_id])
-            edge_density = (2 * no_of_edges_in_community[comm_id]) / (vertices_count * (vertices_count - 1))
-            edge_density_of_community[comm_id] = edge_density
-        return edge_density_of_community
-
-
-
-
-
-    director_communities_density = edge_density_weights(layer1IntraEdgeFile,layer1CommunityFile)
-    movie_communities_density = edge_density_weights(layer2IntraEdgeFile, layer2CommunityFile)
-    with open(layer12SimpleEdgeBipartiteFile,"r") as fp:
-        if not os.path.exists(os.path.dirname(resultFile)):
-            os.makedirs(os.path.dirname(resultFile))
-        fs = open(resultFile, "w")
-        for items in fp:
-            item = items.strip().split(",")
-            actorComm = item[1]
-            directorComm = item[3]
-            edgesWeight = int(item[4])
-            den1 = get_comm_info(actorComm, director_communities_density)
-            den2 = get_comm_info(directorComm, movie_communities_density)
-            ver1 = no_of_vertices_in_director_communities[actorComm]
-            ver2 = no_of_vertices_in_movie_communities[directorComm]
-            if ver1 > 1 and ver2 > 1:
-                weight = den1 * den2 * (edgesWeight/(ver1 * ver2))
-                if weight != 0:
-                    fs.write("{0},{1},{2},{3},{4}\n".format("1",actorComm,"0", directorComm,weight, "\n"))
-
-
-
-
-
-
+  with open(simpleEdgeBipartiteFile,"r") as fp:
+    if not os.path.exists(os.path.dirname(resultFile)):
+      os.makedirs(os.path.dirname(resultFile))
+    fs = open(resultFile, "w")
+    for items in fp:
+      item = items.strip().split(",")
+      layer1CommID = item[1]
+      layer2CommID = item[3]
+      edgesWeight = int(item[4])
+      ver1 = layer1community_vertices[layer1CommID]
+      ver2 = layer2community_vertices[layer2CommID]
+      if ver1 > 1 and ver2 > 1:
+        if layer1CommID in layer1community_vertices and layer1CommID in layer1community_edges:
+          den1 = (2 * layer1community_edges[layer1CommID])/ ( layer1community_vertices[layer1CommID] * (layer1community_vertices[layer1CommID] - 1) )
+        else:
+          den1 = 0
+        if layer2CommID in layer2community_vertices and layer2CommID in layer2community_edges:
+          den2 = (2 * layer2community_edges[layer2CommID])/ ( layer2community_vertices[layer2CommID] * (layer2community_vertices[layer2CommID] - 1) )
+        else:
+          den2 = 0
+        weight = den1 * den2 * (edgesWeight/(ver1 * ver2))
+        if weight != 0:
+          fs.write("{0},{1},{2},{3},{4}\n".format("1",layer1CommID,"0",layer2CommID,weight, "\n"))
--- src/apps/analysis/finalComponents/edgeFractionWeightBipartiteGraphGenerator.py
+++ src/apps/analysis/finalComponents/edgeFractionWeightBipartiteGraphGenerator.py
@@ -1,74 +1,66 @@
 import os
-def get_comm_info(community_id, community_dict):
-    community_id = str(community_id)
-    if community_id in community_dict:
-        return community_dict[community_id]
-    else:
-        return 0
 
-def edgeFractionWeightedBipartiteGraphGenerator(layer1CommunityFile,
-                                      layer2CommunityFile,
-                                      layer12SimpleEdgeBipartiteFile,
-                                      resultFile):
+def edge_fraction_bgg(
+  layer1,
+  layer2,
+  layer1CommunityFile,
+  layer2CommunityFile,
+  layer12SimpleEdgeBipartiteFile,
+  simpleEdgeBipartiteFile,
+  resultFile
+):
 
-    director_community_info = []
-    director_community_dict = {}
-    movie_community_info = []
-    movie_community_dict = {}
-    no_of_vertices_in_movie_communities = {}
-    no_of_vertices_in_director_communities = {}
-    with open(layer1CommunityFile) as f:
-        for line in f:
-            director_community_info.append(line.split(' '))
-        for i in director_community_info:
-            if (len(i) == 3):
-                director_community_dict[i[0]] = i[1]
-                if i[1] not in no_of_vertices_in_director_communities:
-                    no_of_vertices_in_director_communities[i[1]] = 1
-                else:
-                    no_of_vertices_in_director_communities[i[1]]+=1
+  layer1community_info = []
+  layer1community_dict = {}
 
-    with open(layer2CommunityFile) as f:
-        for line in f:
-            movie_community_info.append(line.split(' '))
-        for i in movie_community_info:
-            if (len(i) == 3):
-                movie_community_dict[i[0]] = i[1]
-                if i[1] not in no_of_vertices_in_movie_communities:
-                    no_of_vertices_in_movie_communities[i[1]] = 1
-                else:
-                    no_of_vertices_in_movie_communities[i[1]]+=1
+  layer2community_info = []
+  layer2community_dict = {}
 
+  layer1community_vertices = {}
+  layer2community_vertices = {}
 
+  i = 0
+  with open(layer1CommunityFile) as f:
+    for line in f:
+      i = i + 1
+      line_data = line.strip().split(',')
+      if (i >= 8):
+        layer1community_dict[line_data[0]] = line_data[1]
+        if line_data[1] not in layer1community_vertices:
+          layer1community_vertices[line_data[1]] = 1
+        else:
+          layer1community_vertices[line_data[1]]+=1
+  i = 0
+  with open(layer2CommunityFile) as f:
+    for line in f:
+      i = i + 1
+      line_data = line.strip().split(',')
+      if (i >= 8):
+        layer2community_dict[line_data[0]] = line_data[1]
+        if line_data[1] not in layer2community_vertices:
+          layer2community_vertices[line_data[1]] = 1
+        else:
+          layer2community_vertices[line_data[1]]+=1
 
-
-
-
-
-    with open(layer12SimpleEdgeBipartiteFile,"r") as fp:
-        if not os.path.exists(os.path.dirname(resultFile)):
-            os.makedirs(os.path.dirname(resultFile))
-        fs = open(resultFile, "w")
-        for items in fp:
-            item = items.strip().split(",")
-            actorComm = item[1]
-            directorComm = item[3]
-            edgesWeight = int(item[4])
-            if actorComm in no_of_vertices_in_director_communities:
-                ver1 = no_of_vertices_in_director_communities[actorComm]
-            else:
-                ver1 = 0
-            if directorComm in no_of_vertices_in_movie_communities:
-                ver2 = no_of_vertices_in_movie_communities[directorComm]
-            else:
-                ver2 = 0
-            if ver1 > 1 and ver2 > 1:
-                weight = (edgesWeight/(ver1 * ver2))
-                if weight != 0:
-                    fs.write("{0},{1},{2},{3},{4}\n".format("1",actorComm,"0", directorComm,weight, "\n"))
-
-
-
-
-
-
+            
+  with open(layer12SimpleEdgeBipartiteFile,"r") as fp:
+    if not os.path.exists(os.path.dirname(resultFile)):
+      os.makedirs(os.path.dirname(resultFile))
+    fs = open(resultFile, "w")
+    for items in fp:
+      item = items.strip().split(",")
+      comm1 = item[1]
+      comm2 = item[3]
+      edgesWeight = int(item[4])
+      if comm1 in layer1community_vertices:
+        ver1 = layer1community_vertices[comm1]
+      else:
+        ver1 = 0
+      if comm2 in layer2community_vertices:
+        ver2 = layer2community_vertices[comm2]
+      else:
+        ver2 = 0
+      if ver1 > 1 and ver2 > 1:
+        weight = (edgesWeight/(ver1 * ver2))
+        if weight != 0:
+          fs.write("{0},{1},{2},{3},{4}\n".format("1",comm1,"0",comm2,weight, "\n"))
